96799b08e563751e2ba8d7f0f67a58bf1a89e5b1
FRma1193 <frma1193@gmail.com>
2015-05-14 14:59:18 +0200

    CollisionTiles hinzugefügt, und auch Pathfinding-Alg., funktioniert aber (noch) nicht

---
+++ game/Enemy.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Felix on 14.05.15.
+ */
+Enemy = function () {
+
+
+    this.health=null;
+    this.armor=null;
+    this.speed=null;
+    this.immunityNormal=null;
+    this.immunityAoE=null;
+    this.immunityChain=null;
+    this.immunitySlow=null;
+
+
+
+};
+
+Enemy.prototype = {
+
+
+};
--- states/Level1.js
+++ states/Level1.js
@@ -4,14 +4,20 @@
 
 
 Menu.Level1 = function(game){
+    this.score=0;
+
+
 
 }
 
 var map;
 var layer;
-var player;
+var pathfinder;
 var cursors;
-var counter=0;
+var sprite;
+var marker;
+var blocked = false;
+
 Menu.Level1.prototype = {
 
 
@@ -21,87 +27,116 @@ Menu.Level1.prototype = {
 
          this.load.tilemap('map', 'assets/tilemaps/csv/newMap.csv', null, Phaser.Tilemap.CSV);
          this.load.image('tiles', 'assets/tilemaps/tiles/grass-tiles-2-small.png');
-
-        this.load.spritesheet('player','assets/sprites/stormlord-dragon96x64.png',96,64);
-
-
+         this.load.spritesheet('player', 'assets/sprites/spaceman.png', 16, 16);
          },
 
 
 
-    create: function () {
+    create: function (game) {
 
 
+        this.physics.startSystem(Phaser.Physics.ARCADE);
 
+        //Load Map
          map = this.add.tilemap('map', 64, 64);
          map.addTilesetImage('tiles');
+        // SetCollision-Tiles
          map.setCollision(0);
          //  Create our layer
          layer = map.createLayer(0);
          //  Resize the world
          layer.resizeWorld();
 
-        player = this.add.sprite(900, 190, 'player',7);
+        //Define walkable Tilesets
+        var walkables = [3];
 
-        player.animations.add('left',[8,9],10,true);
-        player.animations.add('right',[8,9],10,true);
-        player.animations.add('up',[8,9],10,true);
-        player.animations.add('down',[8,9],10,true);
-        this.physics.enable(player, Phaser.Physics.ARCADE);
 
-        player.body.setSize(2, 3, 2, 1);
+
+        //Pathfinder -> ADD
+        pathfinder = game.plugins.add(Phaser.Plugin.PathFinderPlugin);
+        pathfinder.setGrid(map.layers[0].data, walkables);
+
+
+        //Add Enemy
+        sprite = this.add.sprite(900, 190, 'player', 1);
+        sprite.animations.add('left', [8,9], 10, true);
+        sprite.animations.add('right', [1,2], 10, true);
+        sprite.animations.add('up', [11,12,13], 10, true);
+        sprite.animations.add('down', [4,5,6], 10, true);
+        //sprite.body.setSize(10, 14, 2, 1);
+        this.physics.enable(sprite, Phaser.Physics.ARCADE);
+
         cursors = this.input.keyboard.createCursorKeys();
+
+        marker = this.add.graphics();
+        marker.lineStyle(2, 0x000000, 1);
+        marker.drawRect(0, 0, 32, 32);
+
+        this.findPathTo(0,5);
 
 
     },
 
     update: function () {
-        var coll =this.physics.arcade.collide(player, layer);
 
-        player.body.velocity.set(0);
-        counter=counter+1;
 
-        if(counter<170){
-            player.body.velocity.x = -100;
-            player.play('left');
+        this.physics.arcade.collide(sprite, layer);
+
+        sprite.body.velocity.x = 0;
+        sprite.body.velocity.y = 0;
+        sprite.body.angularVelocity = 0;
+
+        if (cursors.left.isDown)
+        {
+            sprite.body.velocity.x = -200;
         }
-        else{
-            if(counter<280){
-                player.body.velocity.y=100;
-                player.play('down');
-            }
-            else{
+        else if (cursors.right.isDown)
+        {
+            sprite.body.velocity.x = 200;
+        }
 
-                if(counter<470){
-                    player.body.velocity.x=-100;
-                    player.play('left');
-                }
-                else{
-                    if(counter<630){
-                        player.body.velocity.y=-100;
-                        player.play("up");
-                    }
-                    else{
-                        player.body.velocity.x=-100;
-                        player.play("left");
-                    }
-                }
-
-            }
+        else if (cursors.up.isDown)
+        {
+            sprite.body.velocity.y=-200;
+        }
+        else if(cursors.down.isDown){
+            sprite.body.velocity.y=200;
         }
 
 
+        marker.x = layer.getTileX(this.input.activePointer.worldX) * 32;
+        marker.y = layer.getTileY(this.input.activePointer.worldY) * 32;
+
+
+        if (this.input.mousePointer.isDown)
+        {
+            blocked = true;
+            this.findPathTo(layer.getTileX(marker.x), layer.getTileY(marker.y));
+        }
 
 
     },
 
-    addTower: function () {
+
+    addTower: function (x,y) {
 
 
+    },
 
+ findPathTo: function(tilex, tiley) {
 
+    pathfinder.setCallbackFunction(function(path) {
+        path = path || [];
+        for(var i = 0, ilen = path.length; i < ilen; i++) {
+            map.putTile(46, path[i].x, path[i].y);
+        }
+        blocked = false;
+    });
 
-    }
+    pathfinder.preparePathCalculation([0,0], [tilex,tiley]);
+    pathfinder.calculatePath();
+
+ }
 
 
 
