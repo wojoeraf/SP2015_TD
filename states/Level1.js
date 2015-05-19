cfc35934853802d6ebcfc0502926d48dd1ce4eeb
FRma1193 <frma1193@gmail.com>
2015-05-19 15:49:22 +0200

    Scores, XP-Anzeige und weitere hinzugefügt

--- states/Level1.js
+++ states/Level1.js
@@ -4,20 +4,17 @@
 
 
 Menu.Level1 = function(game){
-    this.score=0;
+
 
 
 }
 
 var map;
 var layer;
-var pathfinder;
-var cursors;
 var sprite;
 var sprite2;
 var sprite3;
 var marker=null;
-var blocked = false;
 var myPoint1 = new Phaser.Point(600,190);
 var myPoint2 = new Phaser.Point(600,400);
 var myPoint3 = new Phaser.Point(300,400);
@@ -25,8 +22,18 @@ var myPoint4 = new Phaser.Point(300,150);
 var myPoint5 = new Phaser.Point(0,150);
 var bool = false;
 var array=[0,0,0];
-var addCounter=0;
-
+var score = 1;
+var scoreText;
+var xpBar;
+var life = 5;
+var heart;
+var heartText;
+var diamond;
+var diamonds=10;
+var diamondText;
+var coin;
+var coins = 100;
+var coinText;
 
 Menu.Level1.prototype = {
 
@@ -41,8 +48,11 @@ Menu.Level1.prototype = {
          this.load.spritesheet('player2', 'assets/sprites/spaceman2.png',16,16);
          this.load.spritesheet('player3', 'assets/sprites/spaceman3.png', 16,16);
          this.load.spritesheet('tower', 'assets/sprites/block.png',32,32);
-        this.load.spritesheet('tower1', 'assets/sprites/Tower1.png');
-
+         this.load.spritesheet('tower1', 'assets/sprites/Tower1.png');
+         this.load.spritesheet('xpBar2', 'assets/sprites/xpBar2.png');
+         this.load.spritesheet('heart', 'assets/sprites/heart.png');
+        this.load.spritesheet('diamond','assets/sprites/diamond.png');
+        this.load.spritesheet('coin', 'assets/sprites/coin1.png');
 
          },
 
@@ -63,56 +73,39 @@ Menu.Level1.prototype = {
          //  Resize the world
          layer.resizeWorld();
 
-        //Define walkable Tilesets
-        var walkables = [3];
+
+
+        scoreText = this.add.text(800,20,"Score: " +score);
 
 
 
-        //Pathfinder -> ADD
-        pathfinder = game.plugins.add(Phaser.Plugin.PathFinderPlugin);
-        pathfinder.setGrid(map.layers[0].data, walkables);
+        this.add.text(400,20, "XP: ");
+        xpBar =  this.add.image(470, 30, 'xpBar2');
+        xpBar.scale.set(0.2);
+        xpBar.scale.x=0.1;
 
 
-        //Add Enemies
-        sprite = this.add.sprite(997, 210, 'player', 1);
-        sprite.animations.add('left', [8,9], 10, true);
-        sprite.animations.add('right', [1,2], 10, true);
-        sprite.animations.add('up', [11,12,13], 10, true);
-        sprite.animations.add('down', [4,5,6], 10, true);
-        sprite.speed=55;
-        sprite.visible=false;
-        //sprite.body.setSize(10, 14, 2, 1);
-        this.physics.enable(sprite, Phaser.Physics.ARCADE);
+        coin = this.add.image(60,22,'coin',1);
+        coin.scale.set(0.9);
+        coinText=this.add.text(100,20,coins);
+
+
+        diamond = this.add.sprite(160,22,'diamond');
+        diamond.scale.set(0.9);
+        diamondText = this.add.text(200,20,diamonds);
 
 
 
-        sprite2 = this.add.sprite(1008, 240, 'player2', 1);
-        sprite2.animations.add('left', [8,9], 10, true);
-        sprite2.animations.add('right', [1,2], 10, true);
-        sprite2.animations.add('up', [11,12,13], 10, true);
-        sprite2.animations.add('down', [4,5,6], 10, true);
-        sprite2.speed=50;
-        sprite2.visible=false;
 
-        this.physics.enable(sprite2, Phaser.Physics.ARCADE);
+        heart= this.add.sprite(250,22,'heart');
+        heart.scale.set(0.5);
+        heartText = this.add.text(290,20,life);
 
 
-        sprite3= this.add.sprite(1000, 190, 'player3', 1);
-        sprite3.animations.add('left', [8,9], 10, true);
-        sprite3.animations.add('right', [1,2], 10, true);
-        sprite3.animations.add('up', [11,12,13], 10, true);
-        sprite3.animations.add('down', [4,5,6], 10, true);
-        sprite3.speed=80;
-        sprite3.visible=false;
 
-        this.physics.enable(sprite3, Phaser.Physics.ARCADE);
 
         this.add.button(850,630,'buttonPlay',this.boolF,this);
-        this.add.button(150,630,'tower1',this.addTower,this);
-
-
-
-
+        this.add.button(50,630,'tower1',this.addTower,this);
 
 
 
@@ -132,9 +125,12 @@ Menu.Level1.prototype = {
                 this.nextWave(sprite2,1);
 
             }
+
             if(array[2]!=5){
                 this.nextWave(sprite3,2);
             }
+
+
 
 
 
@@ -263,6 +259,21 @@ Menu.Level1.prototype = {
                 if(player.x<myPoint5.x+0.5){
                     player.destroy();
                     array[arraynumber]=5;
+                   // score = score + 100;
+                   // scoreText.destroy();
+                   // scoreText = this.add.text(880,20,"Score: " +score);
+                   xpBar.scale.x=xpBar.scale.x+0.01;
+
+                    if((life-1)>=0){
+                    life = life-1;
+                    heartText.destroy();
+                    heartText = this.add.text(290,20,life);
+                    }
+
+                    if(life==0){
+                        this.add.text(350,300,"GAME OVER");
+                        //this.state.start('GameOverMenu');
+                    }
                     break;
                 }
                 break;
@@ -277,13 +288,48 @@ Menu.Level1.prototype = {
     },
 
     boolF : function(){
-        if(bool==false){
-            bool=true;
+
+
+            sprite = this.add.sprite(997, 210, 'player', 1);
+            sprite.animations.add('left', [8,9], 10, true);
+            sprite.animations.add('right', [1,2], 10, true);
+            sprite.animations.add('up', [11,12,13], 10, true);
+            sprite.animations.add('down', [4,5,6], 10, true);
+            sprite.speed=55;
+            sprite.visible=false;
+            //sprite.body.setSize(10, 14, 2, 1);
+            this.physics.enable(sprite, Phaser.Physics.ARCADE);
+
+
+
+            sprite2 = this.add.sprite(1008, 240, 'player2', 1);
+            sprite2.animations.add('left', [8,9], 10, true);
+            sprite2.animations.add('right', [1,2], 10, true);
+            sprite2.animations.add('up', [11,12,13], 10, true);
+            sprite2.animations.add('down', [4,5,6], 10, true);
+            sprite2.speed=50;
+            sprite2.visible=false;
+
+            this.physics.enable(sprite2, Phaser.Physics.ARCADE);
+
+
+            sprite3= this.add.sprite(1000, 190, 'player3', 1);
+            sprite3.animations.add('left', [8,9], 10, true);
+            sprite3.animations.add('right', [1,2], 10, true);
+            sprite3.animations.add('up', [11,12,13], 10, true);
+            sprite3.animations.add('down', [4,5,6], 10, true);
+            sprite3.speed=80;
+            sprite3.visible=false;
+
+            this.physics.enable(sprite3, Phaser.Physics.ARCADE);
+            array=[0,0,0];
+
+
+
+            bool = true;
         }
-        else{
-            bool=false;
-        }
-    }
+
+
 
 
 
